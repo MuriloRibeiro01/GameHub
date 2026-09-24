fefc77befb59891dec17477e9a996f97f7f09a63
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-09-24 00:09:28 -0300

    feat: tela de agendamento de partida.

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -4,10 +4,39 @@ import { useRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomePage() {
+
+    const router = useRouter();
+
+    function agendar() {
+
+        router.replace('/agendar');
+
+    }
     return (
         <ThemedView style={styles.root}>
             <SafeAreaView style={styles.homeContainer}>
+                <View style={styles.welcome}>
+                    <Image source={require('../../public/GameHubImages/profile-pic.png')}/>
+                    <View style={styles.greetingsContainer}>
+                        <Text style={styles.greetings}>
+                            Olá, Tiago
+                        </Text>
+                        <Text style={styles.subGreetings}>
+                            Hoje é dia de vitória
+                        </Text>
+                    </View>
+                    <TouchableOpacity onPress={agendar} style={styles.agendarBtn}>
+                        <Text style={styles.btnTextStyle}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.gameMode}>
 
+                </View>
+                <View style={styles.partidasAgendadas}>
+                    <View style={styles.partidasOpcoes}>
+
+                    </View>
+                </View>
             </SafeAreaView>
         </ThemedView>
     );
@@ -16,8 +45,50 @@ export default function HomePage() {
 const styles = StyleSheet.create({
     homeContainer: {
         backgroundColor: '#0e1645',
+        gap: 50,
     },
     root: {
         flex: 1,
+    },
+    welcome: {
+        justifyContent: 'space-around',
+        flexDirection: 'row',
+    },
+    gameMode: {
+        backgroundColor: 'white',
+        borderColor: 'black',
+        margin: 1,
+    },
+    partidasAgendadas: {
+        backgroundColor: 'white',
+        borderColor: 'black',
+        margin: 1,
+    },
+    partidasOpcoes:{ 
+        backgroundColor: 'blue',
+    },
+    greetings: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20,
+    },
+    subGreetings: {
+        color: 'white',
+
+    },
+    agendarBtn: {
+        backgroundColor: 'red',
+        height: 50,
+        width: 50,
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    btnTextStyle: {
+        fontSize: 30,
+        color: 'white',
+    },
+    greetingsContainer: {
+        paddingRight: 20,
     }
 })
