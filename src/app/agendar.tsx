8fbb0ac675aafb6cea4e437b6b09c79a389e4091
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-09-24 22:52:31 -0300

    feat: primeiras configurações da tela de agendamento.

--- src/app/agendar.tsx
+++ src/app/agendar.tsx
@@ -1,5 +1,27 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { ThemedView } from "@/components/themed-view";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Agendamento() {
-    
+    return (
+        <ThemedView style={styles.root}>
+            <SafeAreaView style={styles.safeContainer}>
+                
+            </SafeAreaView>
+            
+        </ThemedView>
+    );
 }
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+    },
+    safeContainer: {
+        backgroundColor: '#1a245f',
+    },
+    container: {
+        backgroundColor: '#0e1544',
+        height: '100%',
+    }
+});
