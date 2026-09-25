36b891ff53de68f7277d1fac4705d83582ca76d5
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-09-25 14:24:31 -0300

    feat: esqueleto da tela.

--- src/app/agendar.tsx
+++ src/app/agendar.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, Button, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { ThemedView } from "@/components/themed-view";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
@@ -16,21 +16,68 @@ export default function Agendamento() {
             <SafeAreaView style={styles.safeContainer}>
                 <View style={styles.titleBtnContainer}>
                     <TouchableOpacity onPress={voltar} style={styles.voltarBtn}>
-                        <Text>Voltar</Text>
+                        <Text style={styles.voltarBtnText}>Voltar</Text>
                     </TouchableOpacity>
-                    <Text style={styles.textWhite}>Agendar partida</Text>
+                    <Text style={[styles.textWhite, styles.pageTitle, styles.textBold]}>Agendar partida</Text>
+                </View>
+
+                <View style={styles.categoriaContainer}>
+                    <Text style={[styles.textWhite, styles.textBold, styles.pageTitle]}>Categoria</Text>
+                    <View>
+                        <View style={styles.gameMode}>
+                            <View style={styles.scrollBox}>
+                                <Image source={require('../../public/GameHubImages/Icon.png')} style={styles.modeImages}/>
+                                <Text style={styles.scrollBoxText}>Ranqueada</Text>
+                            </View>
+                            <View style={styles.scrollBox}>
+                                <Image source={require('../../public/GameHubImages/Icon(1).png')} style={styles.modeImages}/>
+                                <Text style={styles.scrollBoxText}>Duelo X1</Text>
+                            </View>
+                            <View style={styles.scrollBox}>
+                                <Image source={require('../../public/GameHubImages/Group.png')} style={styles.modeImages}/>
+                                <Text style={styles.scrollBoxText}>Diversão</Text>
+                            </View>
+                        </View>
+                    </View>
                 </View>
 
                 <View>
-                    <Text>Categoria</Text>
+                    <View></View>
+                    <Text>Selecione um servidor</Text>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>
+                            Dia e mês
+                        </Text>
+                        <Text>
+                            Hora e minuto
+                        </Text>
+                    </View>
                     <View>
 
                     </View>
                 </View>
 
                 <View>
-                    
+                    <View>
+                        <Text>
+                            Descrição
+                        </Text>
+                        <Text>
+                            Max 100 caracteres
+                        </Text>
+                    </View>
+
+                    <View>
+                        
+                    </View>
                 </View>
+
+                <TouchableOpacity>
+                    <Text>Agendar</Text>
+                </TouchableOpacity>
             </SafeAreaView>            
         </ThemedView>
     );
@@ -48,15 +95,53 @@ const styles = StyleSheet.create({
         height: '100%',
     },
     voltarBtn: {
-        backgroundColor: 'red'
+        backgroundColor: 'red',
+    },
+    voltarBtnText: {
 
     },
     titleBtnContainer: {
+        padding: 15,
         gap: 15,
-        justifyContent: 'center',
         flexDirection: 'row',
     },
     textWhite: {
         color: 'white',
+    },
+    categoriaContainer: {
+        backgroundColor: '#0e1645',
+    },
+    gameMode: {
+        flexDirection: 'row',
+        backgroundColor: '#0e1645',
+        borderColor: 'black',
+        margin: 10,
+        height: 100,
+        justifyContent: 'space-around',
+    },
+    scrollBox: {
+        backgroundColor: '#1D2766',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#7380d0',
+        height: 100,
+        alignItems: 'center',
+        gap: 5,
+        width: 100,
+    },
+    scrollBoxText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    modeImages: {
+        width: 48,
+        height: 48,
+    },
+    pageTitle: {
+        fontSize: 18,
+    },
+    textBold: {
+        fontWeight: 'bold',
     },
 });
